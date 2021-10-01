5cf3e1c967fe707f36a69c1a447fca99d2e08706
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2021-10-02 01:54:34 +0330

    Added optimizers
    Current optimizers:
    - gradient_decent
    - least_squares_fit

---
+++ optimizers.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def gradient_decent(X: np.array, y: np.array, weight: float, bias: float, n_iterations: int, learning_rate: float) -> tuple:
+    """
+    Performs gradient descent on a given set of data points.
+
+    :param X: Training data.
+    :param y: Target data.
+    :param weight: Weight parameter.
+    :param bias: Bias parameter.
+    :param n_iterations: Number of iterations.
+    :param learning_rate: Learning rate.
+    :return: Weight and bias parameters.
+    """
+
+    n_features = len(X)
+
+    for _ in range(n_iterations):
+
+        # Make prediction
+        y_pred = np.dot(X, weight) + bias
+
+        # Calculate error
+        error = y - y_pred
+
+        # Calculate gradient descent
+        D_m = -(2 / n_features) * sum(X * (error))     # Partial Derivative for M (Weights)
+        D_c = -(2 / n_features) * sum(error)           # Partial Derivative for C (Bias)
+
+        # Update weights and bias
+        weight -= (learning_rate * D_m)
+        bias -= (learning_rate * D_c)
+    
+    return weight, bias
+
+
+def least_squares_fit(X: np.array, y: np.array) -> tuple:
+    """
+    Performs a least squares fit on a given set of data points.
+
+    :param X: Training data.
+    :param y: Target data.
+    :return: Weight and bias parameters.
+    """
+    
+    # Calculating means
+    X_mean = np.mean(X)
+    Y_mean = np.mean(y)
+
+    num = 0
+    den = 0
+
+    # Calculating numerator and denominator
+    for i in range(len(X)):
+        num += (X[i] - X_mean) * (y[i] - Y_mean)
+        den += (X[i] - X_mean) ** 2
+
+    # Calculating weight
+    weight = num / den
+
+    # Calculating bias
+    bias = Y_mean - weight * X_mean
+
+    return weight, bias
--- regression.py
+++ regression.py
@@ -1,10 +1,11 @@
-import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 from costs import MSE
+from optimizers import gradient_decent, least_squares_fit
+from regularizers import L1_Regularizer, L2_Regularizer
 
 
 class Regression:
@@ -26,24 +27,24 @@ class Regression:
 
         # Parameters
         self.bias = 0
-        self.weights = []
+        self.weight = []
 
         # Metrics
         self.training_mse = []
 
 
-    def initialize_weights(self, n_features: int) -> np.ndarray:
+    def initialize_weight(self, n_features: int) -> np.ndarray:
         '''
-        Initialize the weights of the model
+        Initialize the weight of the model
 
         :param n_features: number of features
-        :return: random weights between 0 and 1
+        :return: random weight between 0 and 1
         '''
 
-        self.weights = np.random.rand()
+        self.weight = np.random.rand()
 
 
-    def fit(self, X: np.array, y: np.array) -> None:
+    def fit(self, X: np.array, y: np.array, use_gradient_decent: bool = True) -> None:
         '''
         Fit the model to the training data X using Gradient Descent
 
@@ -54,29 +55,25 @@ class Regression:
 
         self.n_features = len(X)
 
-        # Initialize weights
-        self.initialize_weights(n_features = self.n_features)
-
-        # Gradient descent optimizaion
-        for _ in range(self.n_iterations):
-
-            # Make prediction on X
-            y_pred = self.predict(X)
-
-            # Calculate error
-            error = y - y_pred
-
-            # Calculate cost
-            mse = MSE(y, y_pred)
-            self.training_mse.append(mse)
-
-            # Calculate gradient descent
-            D_m = -(2 / self.n_features) * sum(X * (error))     # Partial Derivative for M (Weights)
-            D_c = -(2 / self.n_features) * sum(error)           # Partial Derivative for C (Bias)
-
-            # Update weights and bias
-            self.weights -= (self.learning_rate * D_m)
-            self.bias -= (self.learning_rate * D_c)
+        # Initialize weight
+        self.initialize_weight(n_features = self.n_features)
+        
+        if use_gradient_decent:
+            # Gradient descent optimizaion
+            self.weight, self.bias = gradient_decent(
+                X = X,
+                y = y,
+                weight = self.weight,
+                bias = self.bias,
+                n_iterations = self.n_iterations,
+                learning_rate = self.learning_rate,
+            )
+        else:
+            # Least squares optimizaion
+            self.weight, self.bias = least_squares_fit(
+                X = X,
+                y = y,
+            )
     
 
     def predict(self, X: np.array) -> np.array:
@@ -87,7 +84,7 @@ class Regression:
         :return: predicted output
         '''
 
-        return X.dot(self.weights) + self.bias
+        return X.dot(self.weight) + self.bias
 
 
 
@@ -108,12 +105,12 @@ if __name__ == '__main__':
     regressor = Regression(n_iterations = 1000, learning_rate = 0.0001)
 
     # Training the model
-    regressor.fit(X_train, y_train)
+    regressor.fit(X_train, y_train, use_gradient_decent = False)
 
     # Make predictions
     y_pred = regressor.predict(X_test)
 
-    print(regressor.weights, regressor.bias)
+    print(regressor.weight, regressor.bias)
 
     # Plotting the results
     plt.figure(figsize = (5, 3))
@@ -143,11 +140,11 @@ if __name__ == '__main__':
 #         self.epochs = epochs
 #         self.learning_rate = learning_rate
 #         self.verbose = verbose
-#         self.weights = None
+#         self.weight = None
 #         self.bias = None
 #         self.costs = []
 #         self.final_cost = None
-#         self.final_weights = None
+#         self.final_weight = None
 #         self.final_bias = None
 #         self.final_predictions = None
 
@@ -155,22 +152,22 @@ if __name__ == '__main__':
 #         """
 #         Fit the model to the data
 #         """
-#         self.weights = np.random.rand(self.data.shape[1], 1)
+#         self.weight = np.random.rand(self.data.shape[1], 1)
 #         self.bias = np.random.rand(1)
 #         for epoch in range(self.epochs):
 #             for i in range(0, self.data.shape[0], self.batch_size):
 #                 x = self.data[i:i + self.batch_size]
 #                 y = self.targets[i:i + self.batch_size]
-#                 predictions = np.dot(x, self.weights) + self.bias
+#                 predictions = np.dot(x, self.weight) + self.bias
 #                 cost = self.cost(predictions, y)
 #                 self.costs.append(cost)
-#                 self.weights -= self.learning_rate * np.dot(x.T,
+#                 self.weight -= self.learning_rate * np.dot(x.T,
 #                                                              (predictions - y))
 #                 self.bias -= self.learning_rate * (predictions - y).sum()
 #             if self.verbose:
 #                 print("Epoch: {} - Cost: {}".format(epoch, cost))
 #         self.final_cost = cost
-#         self.final_weights = self.weights
+#         self.final_weight = self.weight
 #         self.final_bias = self
     
 #     def cost(self, predictions, y):
